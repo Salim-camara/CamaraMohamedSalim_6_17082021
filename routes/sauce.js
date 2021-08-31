2f5091bcea4adb89f108a5d1ed66dc6ee716dfc6
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-08-31 14:34:31 +0200

    Ajout de la fonction d'authentification

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const saucesControll = require('../controllers/sauce');
 const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
 
 // création des différentes routes attendu par le front-end
-router.get('/', saucesControll.getSauces);
-router.get('/:id', saucesControll.getSauce);
-router.post('/', multer, saucesControll.postSauces);
+router.get('/', auth, saucesControll.getSauces);
+router.get('/:id', auth, saucesControll.getSauce);
+router.post('/', auth, multer, saucesControll.postSauces);
 
 module.exports = router;
